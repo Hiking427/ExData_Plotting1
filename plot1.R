e0f1eeaef642081ff9a5ed78aed513d26679b9d0
Hiking427 <35043434+Hiking427@users.noreply.github.com>
2023-02-26 15:48:39 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,11 @@
+power_consm <- read.table("household_power_consumption.txt",sep=";",header=T)
+
+power_consm$Date <- as.POSIXct( power_consm$Date, format="%d/%m/%Y" )
+
+power_part <- subset(power_consm,Date>='2007-02-01' & Date<='2007-02-02')
+
+power_part$Global_active_power <- as.numeric(power_part$Global_active_power)
+
+hist(power_part$Global_active_power,col='red',main='Global Active Power',xlab='Global Active Power (kilowatts)')
+
+
